8931ad115c9c66fe6098599a7cee60f1ae7430b0
Irsan Jie <irsan@mavenlab.com>
2016-10-15 15:30:53 +0800

    installation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const Express = require('express');
+const router = Express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.redirect("/install");
 });
 
 module.exports = router;
