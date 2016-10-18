e269ac04993f8ebe8ec855a18d786bb8a40a964f
Irsan Jie <irsan@mavenlab.com>
2016-10-19 03:54:47 +0800

    check for "installed"

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const Bunyan = require('bunyan');
+const FS = require('fs');
+const Mongoose = require('mongoose');
+const Redis = require('redis');
 const SocketIO = require('socket.io');
+const SocketIORedis = require('socket.io-redis');
 const Vasync = require('vasync');
 
 const log = Bunyan.createLogger({ name : 'tikva:index' });
 
-const io = SocketIO(80);
+const mode = process.env.MODE ? process.env.MODE : "local";
+PROPERTIES = JSON.parse(FS.readFileSync('./resources/properties.json', 'utf8'))[mode];
+log.info(mode, PROPERTIES);
+
+var installed = JSON.parse(FS.readFileSync('./resources/installed.json', 'utf8')).installed;
+log.info("installed", installed);
+
+Mongoose.connect(PROPERTIES.mongodb); //connect to mongodb
+var redis = Redis.createClient(PROPERTIES.redis.url); //connect to to redis server
+
+const io = SocketIO(3000);
+io.adapter(SocketIORedis({ pubClient: redis, subClient: redis }));
 
 io.on('connection', function (socket) {
     log.info("ON CONNECTED");
+
+    if(!installed) {
+        socket.emit("install", {
+            installed : false
+        });
+    }
 
     socket.on('disconnect', function () {
         log.info("ON DISCONNECTED");
     });
 });
 
-log.info("TIKVA is listening to port 80");
+log.info("TIKVA is listening to port 3000");
